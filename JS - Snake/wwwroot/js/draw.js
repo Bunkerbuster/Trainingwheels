ddadbd8451812fae4896bce0de5ccdd2d6027093
BunkerBuster <m.diederen@adelante-zorggroep.nl>
2020-08-17 07:45:32 +0200

    snake

---
+++ JS - Snake/wwwroot/js/draw.js
@@ -0,0 +1,36 @@
+﻿const canvas = document.querySelector(".canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+
+(function setup() {
+
+    snake = new Snake();
+    snake.draw();
+
+    Fruit = new Fruit();
+    Fruit.pickLocation();
+
+    window.setInterval(() => {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+         Fruit.Draw();
+
+        snake.update();
+        snake.draw();
+
+       
+
+    }, 250);
+
+}());
+
+window.addEventListener("keydown", ((evt) => {
+    const direction = evt.key.replace("Arrow", "");
+    snake.changeDirection(direction);
+}));
